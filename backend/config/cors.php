bfee7ceb286702a72482c11da17903df813ac346
Pankaj <chypankajchy26@gmail.com>
2026-09-10 20:45:01 +0545

    fixed backend cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,11 +3,14 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://ecommerce-website-murex-six.vercel.app',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
 ];
-
